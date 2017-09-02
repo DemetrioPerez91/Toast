c25011c22832c983ab7f0f0fcff5793d088d98d6
Demetrio <dem3perez@gmail.com>
2017-09-02 11:45:05 -0400

    Create toast extension for IOS
    I like the toast function in Android

---
+++ Toast.swift
@@ -0,0 +1,31 @@
+//
+//  toast.swift
+//  iosTest
+//
+//  Created by User on 7/3/17.
+//  Copyright © 2017 Demetrio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIViewController {
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 175, y: self.view.frame.size.height-100, width: 350, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+}
